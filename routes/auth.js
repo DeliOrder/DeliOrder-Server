5722e02ed455fdfa2efea6aac51d0c007953617f
vlveqoo <vlveqoo@gmail.com>
2024-08-25 14:35:43 +0900

    Feat: firebase 이메일 가입 기능 추가

--- routes/auth.js
+++ routes/auth.js
@@ -247,4 +247,24 @@ router.post("/refresh/kakao", async (req, res, next) => {
   }
 });
 
+router.post("/sign-up/local", async (req, res, next) => {
+  try {
+    const { nicknameValue, emailValue } = req.body.signUpFormValue;
+    const existUser = await User.findOne({ emailValue }).lean();
+    if (existUser) {
+      res.status(400).json({ error: "비정상적인 접근입니다." });
+    }
+    await User.create({
+      nickname: nicknameValue,
+      email: emailValue,
+      loginType: "local",
+    });
+
+    res.status(201).json({ message: "회원가입에 성공하였습니다" });
+  } catch (error) {
+    console.error("회원가입에 실패하였습니다.", error);
+    res.status(400).json({ error: "회원가입에 실패하였습니다." });
+  }
+});
+
 module.exports = router;
