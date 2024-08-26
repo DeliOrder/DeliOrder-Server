369b16e2bf846bcfc1d5fc9b76dfae1cfa00797f
vlveqoo <vlveqoo@gmail.com>
2024-08-26 17:33:17 +0900

    Refactor: 라우터 경로 개선

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -3,7 +3,6 @@ const express = require("express");
 const signInRouter = require("./sign-in");
 const signOutRouter = require("./sign-out");
 const signUpRouter = require("./sign-up");
-const refreshRouter = require("./refresh");
 const tokenRouter = require("./token");
 
 const router = express.Router();
@@ -11,7 +10,6 @@ const router = express.Router();
 router.use("/sign-in", signInRouter);
 router.use("/sign-out", signOutRouter);
 router.use("/sign-up", signUpRouter);
-router.use("/refresh", refreshRouter);
 router.use("/token", tokenRouter);
 
 module.exports = router;
